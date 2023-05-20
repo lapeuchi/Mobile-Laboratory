7be157bc8709ef8eb814296f0ef55ef8cb04b57e
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2023-05-20 23:27:55 +0900

    no message

---
+++ Mobile-Laboratory/Assets/Art/Earth Cross Section/Script/DrawLine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DrawLine : MonoBehaviour
+{
+	
+	
+	    public Transform cam;
+    	public GameObject startPoint;
+		public GameObject target;
+		public float LineWidth;
+        private LineRenderer line;
+		public Material lineMaterial;
+		public Transform textMeshTransform;
+	   
+
+    void Start()
+    {
+		
+		
+	
+       // LineRenderer  lineRenderer = GetComponent<LineRenderer> ();
+		
+		
+    }
+	
+	void Update ()
+	
+  {
+	    line = this.gameObject.GetComponent<LineRenderer>();
+		line.SetVertexCount(2);
+		line.material = lineMaterial;
+		
+		
+		
+		line.SetWidth(LineWidth, LineWidth);
+		line.SetPosition (0, startPoint.transform.position);
+        line.SetPosition (1, target.transform.position);
+		
+
+  } 
+  
+}
+
